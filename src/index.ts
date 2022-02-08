3aa1fedc6d86d2dfb51ae9f438721d16dda44251
Todd <luiscatodd@gmail.com>
2022-02-08 14:59:03 -0600

    Added imports

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import passport from 'passport'
+import passportLocal from 'passport-local'
+import cookieParser from 'cookie-parser'
+import session from 'express-session'
+import bcrypt from 'bcrypt'
+import dotenv from 'dotenv'
+
 
 const app = express()
 
 app.listen(8000, () => {
     console.log(`Listening on port ${8000}`)
 })
-
-
-
