8601d2e417743a57ea2f6f8259582f17e45b5f11
Todd <luiscatodd@gmail.com>
2022-02-08 15:31:17 -0600

    App Setup

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,27 @@ import dotenv from 'dotenv'
 //Interfaces
 import * as interfaces from 'interfaces'
 
-
+//App Setup
 const app = express()
+app.use(express.json())
+app.use(cors({origin: 'http://localhost:3000', credentials: true, preflightContinue: true}))
+app.use(
+    session({
+        secret: process.env.SECRET || "thisshouldbeasecret",
+        resave: true,
+        saveUninitialized: true
+    })
+)
+
+app.use(cookieParser())
+app.use(passport.initialize())
+app.use(passport.session())
+
+
+//Routes
+app.get('/', (req,res) => {
+    res.send("HELLO WORLD")
+})
 
 app.listen(8000, () => {
     console.log(`Listening on port ${8000}`)
